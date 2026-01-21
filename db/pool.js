c42ba7e815e6111031acbf3dc067e2adcd946cbc
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2026-01-21 17:01:04 -0500

    Fix pool

--- db/pool.js
+++ db/pool.js
@@ -12,12 +12,22 @@ module.exports = new Pool({
 
 //const { Pool } = require("pg");
 
+
+
+
+const pool = new Pool({
+  connectionString: process.env.DB_URL
+});
+
+module.exports = pool;
+
+/*
 module.exports = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME
-});
+}); */
 
 
